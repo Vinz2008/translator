0d1daa2668efbe350a10352cbed6d1f87a9ae91e
Vinz2008 <clubfunreading@@gmail.com>
2021-08-15 00:39:21 +0200

    passed to pysimplegui

--- translate.py
+++ translate.py
@@ -4,12 +4,19 @@ translator = Translator()
 sg.theme('SystemDefault') 
 layout = [[sg.Text('Translator'), sg.Text(size=(15,1))],
 	  [sg.Input(key='-INPUT-')],
+	  [sg.Button("Translate")],
+	  [sg.Text("", key = "-OUTPUT-")],
           [sg.Button("Exit")]],
 
 window = sg.Window('Translator', layout)
 
 while True:
-    event, values = window.read()
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
+	event, values = window.read()
+	if event == sg.WINDOW_CLOSED or event == 'Quit':
+        	break
+	elif event ==  "Translate":
+		input = values["-INPUT-"]
+		translation = translator.translate(input)
+		window[-OUTPUT-](translation)
+
 window.close()
